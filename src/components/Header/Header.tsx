7c9d614206eb2607c8007c63d20deb3e16a1f7ed
Jesús Alarcón <jesus.alarconfrias@hotmail.com>
2024-01-30 21:01:41 -0600

    globals changed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import { MdDarkMode } from "react-icons/md"
 
 const Header = () => {
     return (
-        <header className="py-10 px-4 container max-w-screen-xl mx-auto test-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+        <header className="py-10 container max-w-screen-xl mx-auto test-xl flex flex-wrap md:flex-nowrap items-center justify-between">
             <div className="flex items-center w-full md:2/3">
-                
+
                 <Link href="/" className="font-black text-tertiary-dark">
                     Hotels
                 </Link>
