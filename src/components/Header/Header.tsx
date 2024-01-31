5b76db43987fd683e3458df2cda37e19ea76f098
Jesús Alarcón <jesus.alarconfrias@hotmail.com>
2024-01-30 20:49:32 -0600

    header and footer created

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,15 @@ import { MdDarkMode } from "react-icons/md"
 
 const Header = () => {
     return (
-        <header className="py-10 px-4 container mx-auto test-xl flex flex-wrap md:flex-nowrap items-center justify-between">
-            <div className="flex items-centerw-full md:2/3">
-                <Link href="/" className="font-black text-[#F27405]">Hotels</Link>
+        <header className="py-10 px-4 container max-w-screen-xl mx-auto test-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+            <div className="flex items-center w-full md:2/3">
+                
+                <Link href="/" className="font-black text-tertiary-dark">
+                    Hotels
+                </Link>
                 <ul className="flex items-center ml-5">
                     <li className="flex items-center">
-                        <Link href="/">
+                        <Link href="/auth">
                             <FaUserCircle className="cursor-pointer" />
                         </Link>
                     </li>
@@ -20,7 +23,13 @@ const Header = () => {
             </div>
             <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
                 <li className="hover:-translate-y-2 duration-500 transition-all">
-
+                    <Link href="/">Home</Link>
+                </li>
+                <li className="hover:-translate-y-2 duration-500 transition-all">
+                    <Link href="/rooms">Rooms</Link>
+                </li>
+                <li className="hover:-translate-y-2 duration-500 transition-all">
+                    <Link href="/">Contact</Link>
                 </li>
 
             </ul>
