398b42ec2460cb485254c4c4b5f051ae6f372c0a
Jesús Alarcón <jesus.alarconfrias@hotmail.com>
2024-01-31 22:06:57 -0600

    Theme provider and context complete

---
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import ThemeContext from "@/context/themeContext";
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+    const themeFromStorage: boolean = typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme") ? JSON.parse(localStorage.getItem("hotel-theme")!) : false
+
+    const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+        <div className={`${darkTheme ? "dark" : ""} min-h-screen`}></div>
+
+
+    </ThemeContext.Provider>
+
+}
