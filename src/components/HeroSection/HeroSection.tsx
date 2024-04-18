53a949b60d25967197cd3a410d9903da1c086b74
Jesús Alarcón <jesus.alarconfrias@hotmail.com>
2024-04-17 20:59:42 -0600

    add the images

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -28,8 +28,35 @@ const HeroSection = () => {
       {/* Images */}
       <div className="md:grid hidden gap-8 grid-cols-1">
         <div className="rounded-2xl overflow-hidden">
-          <Image src="" alt="" width={300} height={300} className="img scale-animation" />
+          <Image
+            src="/images/hero-1.jpeg"
+            alt="hero-1"
+            width={300}
+            height={300}
+            className="img scale-animation" />
         </div>
+
+        <div className="grid grid-cols-2 gap-8 h-48">
+          <div className="rounded-2xl overflow-hidden">
+            <Image
+              src="/images/hero-2.jpeg"
+              alt="hero-2"
+              width={300}
+              height={300}
+              className="img scale-animation" />
+          </div>
+          <div className="rounded-2xl overflow-hidden">
+            <Image
+              src="/images/hero-3.jpeg"
+              alt="hero-3"
+              width={300}
+              height={300}
+              className="img scale-animation" />
+          </div>
+        </div>
+
+
+
       </div>
     </section>)
 };
